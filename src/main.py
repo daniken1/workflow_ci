1251f6bde6d884a2ebe8c56c43f79b7237b546af
Daniken Espinoza <despinoza@teralevel.com>
2024-10-06 14:40:49 +0200

    change in main.py message

--- src/main.py
+++ src/main.py
@@ -3,29 +3,29 @@ from termcolor import colored
 import time
 import os
 
-# Función para imprimir un "Hola, Mundo" bonito
-def hola_mundo_bonito():
+def welcome_bonito():
     # Limpiamos la terminal para un efecto más limpio
     os.system('cls' if os.name == 'nt' else 'clear')
     
-    # Texto en estilo ASCII
-    mensaje = """
-    _    _      _ _        __          __        _     _ 
-   | |  | |    | | |       \ \        / /       | |   | |
-   | |__| | ___| | | ___    \ \  /\  / /__  _ __| | __| |
-   |  __  |/ _ \ | |/ _ \    \ \/  \/ / _ \| '__| |/ _` |
-   | |  | |  __/ | | (_) |    \  /\  / (_) | |  | | (_| |
-   |_|  |_|\___|_|_|\___( )    \/  \/ \___/|_|  |_|\__,_|
-                      |/                                
+    # Texto en estilo ASCII con la palabra "Welcome!"
+    mensaje = r"""
+ __        __   _                             
+ \ \      / /__| | ___ ___  _ __ ___   ___  
+  \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ 
+   \ V  V /  __/ | (_| (_) | | | | | |  __/ 
+    \_/\_/ \___|_|\___\___/|_| |_| |_|\___|  
+                                                   
     """
     
-    # Imprimir el mensaje "Hola, Mundo" con color y negritas
+    # Imprimir el mensaje "Welcome!" con color cian y negritas
     print(colored(mensaje, 'cyan', attrs=['bold']))
+    
+    # Mensaje adicional con color amarillo y negritas
+    print(colored("¡Welcome!", 'yellow', attrs=['bold']))
     
     # Añadimos un efecto de espera antes del final
     time.sleep(2)
-    print(colored("¡Bienvenido al mundo de la programación!", 'yellow', attrs=['bold', 'blink']))
 
 # Llamamos a la función para que imprima el mensaje bonito
 if __name__ == "__main__":
-    hola_mundo_bonito()
+    welcome_bonito()
